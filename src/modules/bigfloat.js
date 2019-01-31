dc6b5cc08a492c54e9a845f59e533d5544a89f49
Quentin <qschroed@gmail.com>
2019-01-31 21:32:17 +0100

    added concept

---
+++ src/modules/bigfloat.js
@@ -0,0 +1,12 @@
+//concept :
+
+/*
+    Make it a function that returns a class based on given params e.g. maxlength, stuff like that
+    then use floating pointarithmetic with more than 8 bytes, make it 16 or 32 even
+*/
+
+function bigFloatFactory(presets) {
+    return class {
+        //this. ... = presets. ...
+    }
+}
