9e3b5146a0773b8f26b53e669fcdcc7ad1df8b05
Quentin <qschroed@gmail.com>
2019-01-31 21:32:40 +0100

    ...

--- src/modules/bigfloat.js
+++ src/modules/bigfloat.js
@@ -3,6 +3,7 @@
 /*
     Make it a function that returns a class based on given params e.g. maxlength, stuff like that
     then use floating pointarithmetic with more than 8 bytes, make it 16 or 32 even
+    also needs to have all the functionality of decimal.js
 */
 
 function bigFloatFactory(presets) {
