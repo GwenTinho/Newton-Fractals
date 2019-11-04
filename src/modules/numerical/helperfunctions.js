e71798081a61636d33c2a8bfa4048881b05a4a8e
qs <qschroed@gmail.com>
2019-11-04 18:30:29 +0100

    first step of implementing non integer exponents

--- src/modules/numerical/helperfunctions.js
+++ src/modules/numerical/helperfunctions.js
@@ -35,8 +35,30 @@ function findRoot(startpoint, f, df, tolerance) {
     return z;
 }
 
+function float2rat(x) { // from http://jonisalonen.com/2012/converting-decimal-numbers-to-ratios/ and https://en.wikipedia.org/wiki/Continued_fraction infinite fractions and convergence
+    const tolerance = 1.0E-6;
+    let h1 = 1;
+    let h2 = 0;
+    let k1 = 0;
+    let k2 = 1;
+    let b = x;
+    do {
+        let a = Math.floor(b);
+        let aux = h1;
+        h1 = a * h1 + h2;
+        h2 = aux;
+        aux = k1;
+        k1 = a * k1 + k2;
+        k2 = aux;
+        b = 1 / (b - a);
+    } while (Math.abs(x - h1 / k1) > x * tolerance);
+
+    return [h1, k1]; // h1 is h_n in the formula == numerator | k1 is k_n in the formula == divisor
+}
+
 export default {
     findIterationsNeeded,
     findRoot,
-    findMaxIterationPerPixel
+    findMaxIterationPerPixel,
+    float2rat
 }
